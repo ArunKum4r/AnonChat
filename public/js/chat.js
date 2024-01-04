3f3eb87835e61affe81cf91f4272bd988714eb49
ArunKum4r <arunvjr107@gmail.com>
2024-01-04 23:10:01 +0530

    merging latest styles from master branch

--- public/js/chat.js
+++ public/js/chat.js
@@ -5,64 +5,59 @@ const $messageForm = document.querySelector('#message-form')
 const $messageFormInput = $messageForm.querySelector('input')
 const $messageFormButton = $messageForm.querySelector('button')
 const $sendLocationButton = document.querySelector('#send-location')
-const $messages = document.querySelector('#messages')
-<<<<<<< HEAD
-const menu = document.querySelector('.menu')
-const sidebar = document.querySelector('.chat__sidebar')
-const roomTitle = document.querySelector('.room')
-const wrapper = document.querySelector('.wrapper')
-=======
->>>>>>> 6dbb6a3a1b491fe968c0ad9517147abb10507e23
+const $messages = document.querySelector("#messages");
+const menu = document.querySelector(".menu");
+const sidebar = document.querySelector(".chat__sidebar");
+const roomTitle = document.querySelector(".room");
+const wrapper = document.querySelector(".wrapper");
 
 // Templates
-const messageTemplate = document.querySelector('#message-template').innerHTML
-const locationMessageTemplate = document.querySelector('#location-message-template').innerHTML
-const sidebarTemplate = document.querySelector('#sidebar-template').innerHTML
+const messageTemplate = document.querySelector("#message-template").innerHTML;
+const locationMessageTemplate = document.querySelector(
+  "#location-message-template"
+).innerHTML;
+const sidebarTemplate = document.querySelector("#sidebar-template").innerHTML;
 
 // Options
-const { username, room } = Qs.parse(location.search, { ignoreQueryPrefix: true })
+const { username, room } = Qs.parse(location.search, {
+  ignoreQueryPrefix: true,
+});
 
-socket.on('message', (message) => {
-    console.log(message)
-    const html = Mustache.render(messageTemplate, {
-        username: message.username,
-        message: message.text,
-        createdAt: moment(message.createdAt).format('h:mm a')
-    })
-    $messages.insertAdjacentHTML('beforeend', html)
-})
+socket.on("message", (message) => {
+  console.log(message);
+  const html = Mustache.render(messageTemplate, {
+    username: message.username,
+    message: message.text,
+    createdAt: moment(message.createdAt).format("h:mm a"),
+  });
+  $messages.insertAdjacentHTML("beforeend", html);
+});
 
-socket.on('locationMessage', (message) => {
-    console.log(message)
-    const html = Mustache.render(locationMessageTemplate, {
-        username: message.username,
-        url: message.url,
-        createdAt: moment(message.createdAt).format('h:mm a')
-    })
-    $messages.insertAdjacentHTML('beforeend', html)
-})
+socket.on("locationMessage", (message) => {
+  console.log(message);
+  const html = Mustache.render(locationMessageTemplate, {
+    username: message.username,
+    url: message.url,
+    createdAt: moment(message.createdAt).format("h:mm a"),
+  });
+  $messages.insertAdjacentHTML("beforeend", html);
+});
 
-socket.on('roomData', ({ room, users }) => {
-<<<<<<< HEAD
-    roomName = room
-=======
->>>>>>> 6dbb6a3a1b491fe968c0ad9517147abb10507e23
-    const html = Mustache.render(sidebarTemplate, {
-        room,
-        users
-    })
-    document.querySelector('#sidebar').innerHTML = html
-<<<<<<< HEAD
-    roomTitle.innerText = room
-})
+socket.on("roomData", ({ room, users }) => {
+  roomName = room;
+  const html = Mustache.render(sidebarTemplate, {
+    room,
+    users,
+  });
+  document.querySelector("#sidebar").innerHTML = html;
+  roomTitle.innerText = room;
+});
 
-menu.addEventListener('click', () => {
-    sidebar.classList.toggle('hide')
-    wrapper.classList.toggle('wrapperHide')
-    roomTitle.classList.toggle('hide')
-=======
->>>>>>> 6dbb6a3a1b491fe968c0ad9517147abb10507e23
-})
+menu.addEventListener("click", () => {
+  sidebar.classList.toggle("hide");
+  wrapper.classList.toggle("wrapperHide");
+  roomTitle.classList.toggle("hide");
+});
 
 $messageForm.addEventListener('submit', (e) => {
     e.preventDefault()
